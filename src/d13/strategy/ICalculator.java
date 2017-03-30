8d6e0135bb33a87863a97a63e1756635be42d2c3
xuzhenkang <xuzhenkang@hotmail.com>
2017-03-30 13:55:34 +0800

    策略模式

---
+++ src/d13/strategy/ICalculator.java
@@ -0,0 +1,5 @@
+package d13.strategy;
+
+public interface ICalculator {
+	public int calculate(String exp);
+}
---
+++ src/d13/strategy/StrategyTest.java
@@ -0,0 +1,15 @@
+package d13.strategy;
+
+public class StrategyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String exp = "2+8";
+		ICalculator cal = new Plus();
+		int result = cal.calculate(exp);
+		System.out.println(result);
+	}
+
+}
