e5117ad5ce853c86149e285138f92d8d2c94e778
xuzhenkang <xuzhenkang@hotmail.com>
2017-03-30 13:55:50 +0800

    模板方法模式

---
+++ src/d14/template_method/Minus.java
@@ -0,0 +1,10 @@
+package d14.template_method;
+
+public class Minus extends AbstractCalculator {
+
+	@Override
+	public int calculate(int num1, int num2) {
+		return num1 - num2;
+	}
+
+}
---
+++ src/d14/template_method/Plus.java
@@ -0,0 +1,10 @@
+package d14.template_method;
+
+public class Plus extends AbstractCalculator {
+
+	@Override
+	public int calculate(int num1, int num2) {
+		return num1 + num2;
+	}
+
+}
