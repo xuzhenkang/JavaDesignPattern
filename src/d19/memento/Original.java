3358920abf330b13af78e319dec24797baab1524
xuzhenkang <xuzhenkang@hotmail.com>
2017-03-30 14:09:34 +0800

    备忘录模式

---
+++ src/d19/memento/Original.java
@@ -0,0 +1,22 @@
+package d19.memento;
+
+public class Original {
+	private String value;
+	public Original(String value) {
+		this.value = value;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public Memento createMemento() {
+		return new Memento(value);
+	}
+	 
+	public void restoreMemento(Memento memento) {
+		this.value = memento.getValue();
+	}
+}
---
+++ src/d19/memento/Storage.java
@@ -0,0 +1,15 @@
+package d19.memento;
+
+public class Storage {
+	private Memento memento;
+	public Storage(Memento memento) {
+		this.memento = memento;
+	}
+	public Memento getMemento() {
+		return memento;
+	}
+	public void setMemento(Memento memento) {
+		this.memento = memento;
+	}
+}
+ 
